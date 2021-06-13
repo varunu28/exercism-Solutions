1d7047380bdee99ef54996295ba7210d564c8057
varunu28 <varun.u28@gmail.com>
2021-06-13 11:37:18 -0700

    Added rna-transcription

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,22 @@
+import java.util.Map;
+import java.util.stream.Collector;
+
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        Map<Character, Character> encoding = Map.of(
+            'G', 'C',
+            'C', 'G',
+            'T', 'A',
+            'A', 'U'
+        );
+        return dnaStrand.chars().mapToObj(c -> (char) c).map(encoding::get)
+            .collect(
+                Collector.of(
+                    StringBuilder::new,
+                    StringBuilder::append,
+                    StringBuilder::append,
+                    StringBuilder::toString
+                ));
     }
-
 }
